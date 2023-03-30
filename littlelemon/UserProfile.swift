335f5bd82fa7c250fe342256fb4328f849548722
Milo Edelbi <milo@edelbi.de>
2023-03-30 23:40:26 +0200

    Worked on Onboarding, Networking and Overall Functionality.

---
+++ littlelemon/UserProfile.swift
@@ -0,0 +1,44 @@
+//
+//  UserProfile.swift
+//  littlelemon
+//
+//  Created by Milo Edelbi on 29.03.23.
+//
+
+import SwiftUI
+
+struct UserProfile: View {
+    
+    @Environment(\.presentationMode) var presentation
+    
+    let firstName = UserDefaults.standard.string(forKey: firstNameKey)
+    let lastName = UserDefaults.standard.string(forKey: lastNameKey)
+    let email = UserDefaults.standard.string(forKey: emailKey)
+    var body: some View {
+        VStack{
+            Text("Personal Information")
+            Image("profile-image-placeholder")
+            Text(firstName ?? "?")
+            Text(lastName ?? "?")
+            Text(email ?? "?")
+            
+            Button {
+                UserDefaults.standard.setValue(false, forKey: isLoggedInKey)
+                UserDefaults.standard.removeObject(forKey: firstNameKey)
+                UserDefaults.standard.removeObject(forKey: lastNameKey)
+                UserDefaults.standard.removeObject(forKey: emailKey)
+                self.presentation.wrappedValue.dismiss()
+            } label: {
+                Text("Logout")
+            }
+            
+            Spacer()
+        }
+    }
+}
+
+struct UserProfile_Previews: PreviewProvider {
+    static var previews: some View {
+        UserProfile()
+    }
+}
