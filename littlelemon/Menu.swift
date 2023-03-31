b633ca6757227bb09007f04b8f79992ae652164b
Milo Edelbi <milo@edelbi.de>
2023-03-31 20:35:34 +0200

    Started working on the getMenuData() Function and added the ManagedObjectContext to the Menu Struct as an enviornment variable

--- littlelemon/Menu.swift
+++ littlelemon/Menu.swift
@@ -8,6 +8,9 @@
 import SwiftUI
 
 struct Menu: View {
+    
+    @Environment(\.managedObjectContext) private var viewContext
+    
     var body: some View {
         VStack{
             Text("Little Lemon")
@@ -17,6 +20,23 @@ struct Menu: View {
                 
             }
         }
+        .onAppear{
+            getMenuData()
+        }
+    }
+    
+    func getMenuData() {
+        let serverURL = "https://raw.githubusercontent.com/Meta-Mobile-Developer-PC/Working-With-Data-API/main/menu.json"
+        let url = URL(string: serverURL)!
+        let request = URLRequest(url: url)
+        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+            if let error = error {
+                print("Error: \(error.localizedDescription)")
+            } else if let data = data {
+                let fullMenu = try? JSONDecoder().decode(JSONMenu.self, from: data)
+            }
+        }
+        task.resume()
     }
 }
 
